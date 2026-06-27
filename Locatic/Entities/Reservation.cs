f3aafd10cf326041c000a54d9d7e089db02deb8d
Esso <assiahlouange@gmail.com>
2026-06-27 08:42:47 +0000

    refactor: add entity validations

--- Locatic/Entities/Reservation.cs
+++ Locatic/Entities/Reservation.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Locatic.Entities;
 
 public class Reservation
 {
     public int Id { get; set; }
 
+    [Required]
     public DateTime StartDate { get; set; }
 
+    [Required]
     public DateTime EndDate { get; set; }
 
+    [Range(1, int.MaxValue)]
     public int ClientId { get; set; }
+
     public Client Client { get; set; } = null!;
 
+    [Range(1, int.MaxValue)]
     public int CarId { get; set; }
+
     public Car Car { get; set; } = null!;
 }
